a0493d775f3dacbe39c5c2b4eb139b23eea7846f
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-07 13:15:28 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App></App>)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
