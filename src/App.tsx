7fc16c03b062272d8759a091cb28d43c5f38ca09
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-14 11:32:54 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import AuthLayout from './_auth/AuthLayout'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import RootLayout from './_root/RootLayout'
-import { Home } from './_root/pages/Home'
+import { Home } from './_root/pages'
+
 import './globals.css'
 import { Routes, Route } from 'react-router-dom'
 const App = () => {
