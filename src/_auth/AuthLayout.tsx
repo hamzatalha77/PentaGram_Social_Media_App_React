8dd515cbd89868c8e85400263196ef21f079edc7
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-14 11:52:16 +0100

    commit

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,20 @@
+import { Outlet, Navigate } from 'react-router-dom'
 const AuthLayout = () => {
-  return <div></div>
+  const isAuthenticated = false
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  )
 }
 
 export default AuthLayout
