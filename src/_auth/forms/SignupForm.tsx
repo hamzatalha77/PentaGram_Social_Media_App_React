4e7406be131a406d6709f465319068807e9ab454
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-18 16:35:30 +0100

    commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,7 +2,16 @@ import { Button } from '@/components/ui/button'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
-
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 const formSchema = z.object({
   username: z.string().min(2, {})
 })
