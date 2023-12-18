59c9be2f2e7c0de27fbd3c85afa3a09ed29ba20e
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-18 16:37:16 +0100

    commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -23,8 +23,6 @@ const SignupForm = () => {
     }
   })
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values)
   }
 
