beccd634ac281ffe4365dd5d0f8ce40559ae28d7
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-26 12:54:09 +0100

    commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,7 @@ import { Input } from '@/components/ui/input'
 import { SignupValidation } from '../../lib/validation/index'
 
 const SignupForm = () => {
+  const isLoading = true
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
