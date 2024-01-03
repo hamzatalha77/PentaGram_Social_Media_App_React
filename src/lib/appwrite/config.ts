426a3ba6991077385ab83a95497258a7b960ffeb
hamzatalha77 <hamza_ta7@hotmail.com>
2024-01-03 18:21:39 +0100

    commit

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,4 +1,4 @@
 import { Client, Account, Database, Storage, Avatars } from 'appwrite'
-export const appwrite Config={
-    
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID
 }
