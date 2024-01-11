8222e78da81b5f7ecef512adb9e8a534b1643a4a
hamzatalha77 <hamza_ta7@hotmail.com>
2024-01-11 19:20:52 +0100

    commit

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -2,10 +2,12 @@ import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useEffect } from 'react'
+import { useUserContext } from '../../context/AuthContext'
 
 const Topbar = () => {
   const { mutate: signOut, isSuccess } = useSignOutAccount()
   const navigate = useNavigate()
+  const { user } = useUserContext()
   useEffect(() => {
     if (isSuccess) navigate(0)
   }, [isSuccess])
@@ -28,6 +30,13 @@ const Topbar = () => {
           >
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
+          <Link to={`/profile/${user.id} `} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
